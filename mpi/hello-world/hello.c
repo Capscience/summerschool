de54d69bba7216f96531cda860ec411e80553935
Simo Soini <simo@capscience.fi>
2023-06-28 22:51:59 +0300

    Test runs and exercises

--- mpi/hello-world/hello.c
+++ mpi/hello-world/hello.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <mpi.h>
 
 int main(int argc, char *argv[])
 {
-    // TODO: say hello! in parallel
+    MPI_Init(&argc, &argv);
+
+    int size;
+    int rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
     printf("Hello!\n");
+    printf("We are %d in total!\n", size);
+    fflush(stdout);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    printf("We are rank %d\n", rank);
+    MPI_Finalize();
 }
